2f077cf6fac6953c8576e0bac12265aa304b14a3
JIMMC <57700505+jimmyLeeMc@users.noreply.github.com>
2020-05-19 14:35:00 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -16,18 +16,16 @@ public class Main {
             BufferedImage image = null;
                 //先找出服务器文件排放规律填入url
                 try {
-                    URL url = new URL("https://resources.pearsonactivelearn.com/r00/r0091/r009134/r00913465/current/OPS/images/913465-\"+String.format(\"%03d\",i)+\".jpg");
+                    URL url = new URL("https://resources.pearsonactivelearn.com/r00/r0091/r009134/r00913465/current/OPS/images/913465-"+String.format("%03d",i)+".jpg");
                     //MATH AA https://resources.pearsonactivelearn.com/r00/r0091/r009134/r00913465/current/OPS/images/913465-"+String.format("%03d",i)+".jpg
                     //math AI http://resources.pearsonactivelearn.com/r00/r0091/r009187/r00918716/current/OPS/images/918716-" + i + ".jpg
                     // read the url
                     image = ImageIO.read(url);
-                    
+
                     ImageIO.write(image, "jpg", new File("/Users/jimli/Desktop/DP/书/AAH/"+i+".jpg"));
 
-        } catch (Exception e) {
-        e.printStackTrace();
-        }
-        i=i+1;
+                }catch (Exception e) { e.printStackTrace(); }
+            i++;
         }while (i<15);
-        }
-        }
+    }
+}
